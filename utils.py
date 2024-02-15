fc63e28c3a671621a6e678ff13f25ba8f7aab0d5
Patryk Droś <pdros@splunk.com>
2024-02-15 11:29:41 +0100

    fix!: correct of empty source name if no tickets

--- utils.py
+++ utils.py
@@ -53,7 +53,6 @@ def load_options(sources=[]):
     _tasks = {}
     if "file" in sources:
         source_name = "Local"
-        _tasks[source_name] = []
         print("Loading tasks from file...")
         try:
             with open(const.TASK_LIST_FILENAME, "r") as file:
@@ -66,6 +65,8 @@ def load_options(sources=[]):
                             url=url,
                             summary=summary
                         )
+                        if not _tasks.get(source_name):
+                            _tasks[source_name] = []
                         _tasks[source_name].append(task)
                     else:
                         print(f"{line} not added. It's a # comment!")
@@ -95,9 +96,9 @@ def load_options(sources=[]):
     if 'watcher' in sources:
         print("Loading tasks from JIRA as a watcher...")
         source_name = "Watcher"
-        _tasks[source_name] = []
         _items = jira_helper.get_my_open_issues(watcher=True)  # Should return a list of dicts
         for key in _items.keys():
+            _tasks[source_name] = []
             for item in _items[key]:
                 task = Task(
                     title=f"{item['issue_key']} - {item['summary'][:30]}",
